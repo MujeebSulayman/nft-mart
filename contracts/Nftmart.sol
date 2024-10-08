495f62850fa615987502311f73316151a979132d
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-10-08 16:26:36 +0100

    PayOut and Mint NFT

--- contracts/Nftmart.sol
+++ contracts/Nftmart.sol
@@ -10,10 +10,12 @@ contract Nftmart is ERC721, Ownable, ReentrancyGuard {
   using Counters for Counters.Counter;
   Counters.Counter private _totalNfts;
   Counters.Counter private _totalTokens;
+  Counters.Counter private _totalSales;
 
   uint256 public servicePct;
   uint256 public balance;
 
+  // Nft Struct
   struct NftStruct {
     uint256 id;
     string name;
@@ -22,18 +24,21 @@ contract Nftmart is ERC721, Ownable, ReentrancyGuard {
     address owner;
     uint256 price;
     uint256 timestamp;
+    uint256 endTime;
     bool deleted;
     bool minted;
     bool paidOut;
     bool refunded;
   }
 
+  // Sales Struct
   struct SalesStruct {
     uint256 id;
     uint256 nftId;
     address owner;
     uint256 price;
     uint256 timestamp;
+    uint256 endTime;
     bool minted;
     bool refunded;
   }
@@ -46,18 +51,21 @@ contract Nftmart is ERC721, Ownable, ReentrancyGuard {
     servicePct = _pct;
   }
 
+  // Create Nfts
   function createNft(
     string memory name,
     string memory description,
     string memory imageUrl,
+    uint256 endTime,
     uint256 price
   ) public {
     require(price > 0 ether, 'Price should be greater than zero');
     require(bytes(name).length > 0, 'Name should be greater than zero');
+    require(endTime > currentTime(), 'End time should be greater than current time');
     require(bytes(description).length > 0, 'Description should be greater than zero');
     require(bytes(imageUrl).length > 0, 'ImageUrl should be greater than zero');
 
-    _totalNfts.increament();
+    _totalNfts.increment();
     NftStruct memory nftX;
 
     nftX.id = _totalNfts.current();
@@ -67,16 +75,18 @@ contract Nftmart is ERC721, Ownable, ReentrancyGuard {
     nftX.owner = msg.sender;
     nftX.price = price;
     nftX.timestamp = currentTime();
-
+    nftX.endTime = endTime;
     nftExists[nftX.id] = true;
     nfts[nftX.id] = nftX;
   }
 
+  // Update Nfts
   function updateNft(
     uint256 nftId,
     string memory name,
     string memory description,
     string memory imageUrl,
+    uint256 endTime,
     uint256 price
   ) public {
     require(nftExists[nftId], 'Nft does not exist');
@@ -85,15 +95,16 @@ contract Nftmart is ERC721, Ownable, ReentrancyGuard {
     require(bytes(name).length > 0, 'Name cannot be empty');
     require(bytes(description).length > 0, 'Description cannot be empty');
     require(bytes(imageUrl).length > 0, 'ImageUrl cannot be empty');
-
+    require(endTime > currentTime(), 'End time should be greater than current time');
     nfts[nftId].name = name;
-    nfts[nftsId].description = description;
-    nfts[nftsId].imageUrl = imageUrl;
-    nfts[nftsId].price = price;
+    nfts[nftId].description = description;
+    nfts[nftId].imageUrl = imageUrl;
+    nfts[nftId].price = price;
+    nfts[nftId].endTime = endTime;
   }
 
-
-    funtion deleteNft (uint256 id) public {
+  // Delete Nfts
+  function deleteNft(uint256 id) public {
     require(nftExists[id], 'Nft does not exist');
     require(msg.sender == nfts[id].owner || msg.sender == owner(), 'Unauthorized entity');
     require(!nfts[id].minted, 'Nft already minted');
@@ -103,39 +114,170 @@ contract Nftmart is ERC721, Ownable, ReentrancyGuard {
     nfts[id].deleted = true;
   }
 
-  function refundNfts(uint256 id) internal returns (bool) {
-    for (uint256 i = 0; i < sales[nftsId].length; i++){
-     sales[nftsId][i].refunded = true;
-     payTo(sales[nftsId][i].owner, sales[nftsId][i].price);
-     balance -= sales[nftsId][i].price;
+  // Refund Nfts
+  function refundNfts(uint256 id) internal returns (bool success) {
+    for (uint256 i = 0; i < sales[id].length; i++) {
+      sales[id][i].refunded = true;
+      payTo(sales[id][i].owner, sales[id][i].price);
+      balance -= sales[id][i].price;
     }
+    return true;
   }
- 
- function getSingleNft( uint256 id ) public view returns (NftStruct memory) {
-  return nfts[id];
- }
 
-funtion getAllNfts () public view returns (NftStruct[] memory){
-  uint256 availableNfts;
-  for (uint256 i = 1; i< _totalNfts.current(); i++  ){
-    if(nfts[i].deleted == false){
-      availableNfts++;
+  function getSingleNft(uint256 id) public view returns (NftStruct memory) {
+    return nfts[id];
+  }
+
+  // Get all Nfts
+  function getAllNfts() public view returns (NftStruct[] memory Nfts) {
+    uint256 availableNfts;
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      if (nfts[i].deleted == false) {
+        availableNfts++;
+      }
     }
+    // Initialize Nfts array
+    Nfts = new NftStruct[](availableNfts);
+    uint256 index;
+    // Loop through all Nfts
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      if (nfts[i].deleted == false) {
+        Nfts[index] = nfts[i];
+        index++;
+      }
+    }
+    return Nfts;
   }
 
-  NftStruct[] memory availableNfts = new NftStruct[](availableNfts);
-  uint256 index;
-  for (uint256 i = 1; i< _totalNfts.current(); i++  ){
-    if(nfts[i].deleted == false){
-      availableNfts[index] = nfts[i];
+  // Get all Nfts owned by the user
+  function getMyNfts() public view returns (NftStruct[] memory Nfts) {
+    uint256 availableNfts;
+    // Loop through all Nfts
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      if (!nfts[i].deleted && nfts[i].owner == msg.sender) {
+        availableNfts++;
+      }
+    }
+    // Initialize Nfts array
+    Nfts = new NftStruct[](availableNfts);
+
+    uint256 index;
+    // Loop through all Nfts
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      if (!nfts[i].deleted && nfts[i].owner == msg.sender) {
+        Nfts[index] = nfts[i];
+        index++;
+      }
+    }
+    return Nfts;
   }
 
-    
-function payTo(address to, uint256 amount) internal {
-    (bool success, ) = payable(to).call{value: amount}('');
+  // Buy Nft
+  function buyNft(uint256 nftId) public payable nonReentrant {
+    require(nftExists[nftId], 'Nft does not exist');
+    require(msg.value >= nfts[nftId].price, 'Insufficient funds');
+    require(nfts[nftId].deleted == false, 'Nft already deleted');
+    require(nfts[nftId].minted == false, 'Nft already minted');
+    require(nfts[nftId].endTime > currentTime(), 'Nft sale has ended');
+
+    SalesStruct memory sale;
+    sale.id = sales[nftId].length + 1;
+    sale.nftId = nftId;
+    sale.owner = msg.sender;
+    sale.price = nfts[nftId].price;
+    sale.timestamp = currentTime();
+    sale.endTime = nfts[nftId].endTime;
+    sales[nftId].push(sale);
+    balance += msg.value;
+    nfts[nftId].owner = msg.sender;
+
+    _totalSales.increment();
+  }
+
+  // Get Sales of a Nft
+  function getSales(uint256 nftId) public view returns (SalesStruct[] memory Sales) {
+    return sales[nftId];
+  }
+
+  // Get All Sales
+  function getAllSales() public view returns (SalesStruct[] memory Sales) {
+    uint256 totalSalesCount = 0;
+
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      totalSalesCount += sales[i].length;
+    }
+
+    Sales = new SalesStruct[](totalSalesCount);
+    uint256 index = 0;
+
+    // Flatten sales data into a single array
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      for (uint256 j = 0; j < sales[i].length; j++) {
+        Sales[index] = sales[i][j];
+        index++;
+      }
+    }
+    return Sales;
+  }
+
+  // Get All Sales of a user
+  function getMySales() public view returns (SalesStruct[] memory Sales) {
+    uint256 _totalSalesCount = 0;
+
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      for (uint256 j = 0; j < sales[i].length; j++) {
+        if (sales[i][j].owner == msg.sender) {
+          _totalSalesCount++;
+        }
+      }
+    }
+    Sales = new SalesStruct[](_totalSalesCount);
+    uint256 index = 0;
+    for (uint256 i = 1; i <= _totalNfts.current(); i++) {
+      for (uint256 j = 0; j < sales[i].length; j++) {
+        if (sales[i][j].owner == msg.sender) {
+          Sales[index] = sales[i][j];
+          index++;
+        }
+      }
+    }
+    return Sales;
+  }
+
+  //Payout
+  function payout(uint256 nftId) public {
+    require(nftExists[nftId], 'Nft does not exist');
+    require(msg.sender == nfts[nftId].owner || msg.sender == owner(), 'Unauthorized entity');
+    require(!nfts[nftId].deleted, 'Nft already deleted');
+    require(!nfts[nftId].paidOut, 'Nft already paid out');
+    require(nfts[nftId].minted, 'Nft not minted');
+
+    uint256 payoutAmount = (balance * (10000 - servicePct)) / 10000;
+    uint256 serviceAmount = balance - payoutAmount;
+
+    payTo(nfts[nftId].owner, payoutAmount);
+    payTo(owner(), serviceAmount);
+    nfts[nftId].paidOut = true;
+  }
+
+  //Mint Nft
+  function mintNft(uint256 nftId) internal returns (bool) {
+    for (uint256 i = 0; i < sales[nftId].length; i++) {
+      _totalTokens.increment();
+      sales[nftId][i].minted = true;
+      _mint(sales[nftId][i].owner, _totalTokens.current());
+    }
+    nfts[nftId].minted = true;
+    return true;
+  }
+
+  // Pay to
+  function payTo(address to, uint256 amount) internal {
+    (bool success, ) = payable(to).call{ value: amount }('');
     require(success, 'Payment failed');
   }
 
+  // Get current time
   function currentTime() internal view returns (uint256) {
     return (block.timestamp * 1000) + 1000;
   }
